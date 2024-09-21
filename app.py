a56e557c9467f93032f8105b11000b2cccd502c6
Usama Bhakrani <usamabhakrani@gmail.com>
2024-09-21 20:59:24 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+
+import math
+is_loading = False
+number = 1000
+name = 'Usama Bhakrani'
+
+
+# \"
+# \'
+# \\
+# \n
+
+# print(len(name))
+# print(name[0])
+# print(name[-1])
+# print(name[0:3])
+
+
+first = 'Usama'
+last = 'Bhakrani'
+
+full = f"{len(first)} {last}"
+print(full)
+
+
+print(round(2.9))
