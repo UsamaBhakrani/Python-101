0301f5b75035412f9de9ed1b88c7cf94a7246988
Usama Bhakrani <usamabhakrani@gmail.com>
2024-09-23 18:53:08 +0500

    conrol flow in python

--- app.py
+++ app.py
@@ -20,7 +20,32 @@ first = 'Usama'
 last = 'Bhakrani'
 
 full = f"{len(first)} {last}"
-print(full)
+# print(full)
 
 
-print(round(2.9))
+# print(round(2.9))
+
+
+# Logic
+
+# temperature = 15
+# if temperature > 30:
+#     print('It is too hot.')
+#     print('Please Start the Aircon')
+# elif temperature > 20:
+#     print('Please Stop the Aircon')
+# else:
+#     print('It is cold.')
+#     print('Please Start the Heater')
+
+age = 22
+message = 'Eligible' if age >= 18 else 'Not eligible'
+print(message)
+
+high_income = False
+good_credit = False
+
+if not high_income or not good_credit:
+    print('Eligible for loan')
+else:
+    print('Not eligible for loan')
