2de4f2a0d44cf982907d80ae6929652ea91bcacd
Usama Bhakrani <usamabhakrani@gmail.com>
2024-10-05 14:32:08 +0500

    exceptions

--- app.py
+++ app.py
@@ -337,14 +337,25 @@ full = f"{len(first)} {last}"
 
 
 # Exercise
-sentence = 'This is a common interview question'
-char_frequency = {}
-for x in sentence:
-    if x in char_frequency:
-        char_frequency[x] += 1
-    else:
-        char_frequency[x] = 1
+# sentence = 'This is a common interview question'
+# char_frequency = {}
+# for x in sentence:
+#     if x in char_frequency:
+#         char_frequency[x] += 1
+#     else:
+#         char_frequency[x] = 1
 
-char_frequency_sorted = sorted(
-    char_frequency.items(), key=lambda x: x[1], reverse=True)
-print(char_frequency_sorted[0])
+# char_frequency_sorted = sorted(
+#     char_frequency.items(), key=lambda x: x[1], reverse=True)
+# print(char_frequency_sorted[0])
+
+# Exceptions
+
+try:
+    age = int(input('Age: '))
+    print(age)
+except ValueError:
+    print('Not valid age')
+else:
+    print('no Exception found')
+print('Executing')
