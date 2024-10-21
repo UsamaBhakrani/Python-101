b18ea437ea3fbbf26965d2359caeca6de4869ebd
Usama Bhakrani <usamabhakrani@gmail.com>
2024-10-21 20:16:55 +0500

    constructors

--- app.py
+++ app.py
@@ -387,10 +387,13 @@ full = f"{len(first)} {last}"
 # OOP with Classes
 
 class Point:
-    def draw(self, x, y):
-        print(x, y)
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        print(f'Point ({self.x} {self.y})')
 
 
 point = Point(1, 2)
 point.draw()
-print(isinstance(point, Point))
