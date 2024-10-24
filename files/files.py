2d97f1d57b62a964a87b2f4beeb9c250f6836811
Usama Bhakrani <usamabhakrani@gmail.com>
2024-10-24 07:38:44 +0500

    zipfiles

--- files/files.py
+++ files/files.py
@@ -1,7 +1,16 @@
 from pathlib import Path
 import shutil
-Path('/user/local/bin/')
-path = Path('ecommerce/__init__.py')
-# paths = [p for p in path.iterdir()]
-print(path.read_text())
-shutil.copy()
+from zipfile import ZipFile
+
+# with ZipFile('files.zip', 'w') as zip:
+#     for path in Path('ecommerce').rglob('*.*'):
+#         zip.write(path)
+
+with ZipFile('files.zip') as zip:
+    print(zip.namelist())
+
+    # Path('/user/local/bin/')
+    # path = Path('ecommerce/__init__.py')
+    # # paths = [p for p in path.iterdir()]
+    # print(path.read_text())
+    # shutil.copy()
