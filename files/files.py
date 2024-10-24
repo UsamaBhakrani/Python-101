74cc9dbf9265e1f231c158c69975a78218a3e5d7
Usama Bhakrani <usamabhakrani@gmail.com>
2024-10-24 07:23:57 +0500

    shutil

--- files/files.py
+++ files/files.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-
+import shutil
 Path('/user/local/bin/')
 path = Path('ecommerce/__init__.py')
-print(path.exists())
-print(path.name)
+# paths = [p for p in path.iterdir()]
+print(path.read_text())
+shutil.copy()
