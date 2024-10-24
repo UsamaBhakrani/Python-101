1c54987f52cdd424b37b9678a508ca73f2f12378
Usama Bhakrani <usamabhakrani@gmail.com>
2024-10-24 07:43:58 +0500

    CSV

--- files/files.py
+++ files/files.py
@@ -1,16 +1,29 @@
 from pathlib import Path
 import shutil
 from zipfile import ZipFile
+import csv
 
 # with ZipFile('files.zip', 'w') as zip:
 #     for path in Path('ecommerce').rglob('*.*'):
 #         zip.write(path)
 
-with ZipFile('files.zip') as zip:
-    print(zip.namelist())
+# with ZipFile('files.zip') as zip:
+#     print(zip.namelist())
 
-    # Path('/user/local/bin/')
-    # path = Path('ecommerce/__init__.py')
-    # # paths = [p for p in path.iterdir()]
-    # print(path.read_text())
-    # shutil.copy()
+# Path('/user/local/bin/')
+# path = Path('ecommerce/__init__.py')
+# # paths = [p for p in path.iterdir()]
+# print(path.read_text())
+# shutil.copy()
+
+# CSV Files
+# with open('data.csv', 'w') as file:
+#     writer = csv.writer(file)
+#     writer.writerow(['Name', 'Age', 'City'])
+#     writer.writerow(['John', 30, 'New York'])
+#     writer.writerow(['Jane', 28, 'Los Angeles'])
+
+with open('data.csv') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
