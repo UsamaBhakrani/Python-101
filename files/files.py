8520344aadae5c15efcb1bbb9e545c1bf65b1cbf
Usama Bhakrani <usamabhakrani@gmail.com>
2024-10-28 19:29:59 +0500

    templates in python

--- files/files.py
+++ files/files.py
@@ -98,13 +98,18 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from pathlib import Path
+from string import Template
 import smtplib
+
+template = Template(Path('template.html').read_text())
+
 
 message = MIMEMultipart()
 message['from'] = 'Mosh Hamedani'
 message['to'] = 'geniusmaniacs@gmail.com'
 message['subject'] = 'This is a test message'
-message.attach(MIMEText('Body'))
+body = template.substitute({'name': 'John'})
+message.attach(MIMEText(body, 'html'))
 message.attach(MIMEImage(Path('image.jpg').read_bytes()))
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
