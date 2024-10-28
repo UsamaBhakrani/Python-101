79daf9c29877fa5ff21b5d2620087df7d1152225
Usama Bhakrani <usamabhakrani@gmail.com>
2024-10-28 19:47:28 +0500

    subprocess

--- files/files.py
+++ files/files.py
@@ -94,27 +94,33 @@ import json
 # webbrowser.open('http://nurcouture.uk')
 # webbrowser.open_new('http://nurcouture.uk')
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from pathlib import Path
-from string import Template
-import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+# from email.mime.image import MIMEImage
+# from pathlib import Path
+# from string import Template
+# import smtplib
 
-template = Template(Path('template.html').read_text())
+# template = Template(Path('template.html').read_text())
 
 
-message = MIMEMultipart()
-message['from'] = 'Mosh Hamedani'
-message['to'] = 'geniusmaniacs@gmail.com'
-message['subject'] = 'This is a test message'
-body = template.substitute({'name': 'John'})
-message.attach(MIMEText(body, 'html'))
-message.attach(MIMEImage(Path('image.jpg').read_bytes()))
+# message = MIMEMultipart()
+# message['from'] = 'Mosh Hamedani'
+# message['to'] = 'geniusmaniacs@gmail.com'
+# message['subject'] = 'This is a test message'
+# body = template.substitute({'name': 'John'})
+# message.attach(MIMEText(body, 'html'))
+# message.attach(MIMEImage(Path('image.jpg').read_bytes()))
 
-with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.login('geniusmaniacs@gmail.com', 'password')
-    smtp.send_message(message)
-    print('Test message')
+# with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+#     smtp.ehlo()
+#     smtp.starttls()
+#     smtp.login('geniusmaniacs@gmail.com', 'password')
+#     smtp.send_message(message)
+#     print('Test message')
+
+
+import subprocess
+
+result = subprocess.run(['python3', 'files/other.py'])
+print(result)
